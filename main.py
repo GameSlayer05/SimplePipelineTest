a9009027c38c347851fc08a98fe618337ffed3db
Paysen <paysencalvert@icloud.com>
2026-03-16 15:24:52 -0600

    Fourht Commit

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-def calculator():
+def calculator(num1, num2, operator):
+    if operator == '+':
+        print(f"Result: {num1 + num2}")
+    elif operator == '-':
+        print(f"Result: {num1 - num2}")
+    elif operator == '*':
+        print(f"Result: {num1 * num2}")
+    elif operator == '/':
+        if num2 == 0:
+            print("Error: Division by zero")
+        else:
+            print(f"Result: {num1 / num2}")
+    else:
+        print("Invalid operator")
+
+if __name__ == "__main__":
     try:
         num1 = float(input("Enter first number: "))
         operator = input("Enter operator (+, -, *, /): ")
         num2 = float(input("Enter second number: "))
 
-        if operator == '+':
-            print(f"Result: {num1 + num2}")
-        elif operator == '-':
-            print(f"Result: {num1 - num2}")
-        elif operator == '*':
-            print(f"Result: {num1 * num2}")
-        elif operator == '/':
-            if num2 == 0:
-                print("Error: Division by zero")
-            else:
-                print(f"Result: {num1 / num2}")
-        else:
-            print("Invalid operator")
+        calculator(num1, num2, operator)
+
     except ValueError:
         print("Invalid input: Please enter numerical values.")
-
-calculator()
 
 # Poop
---
+++ test_python.py
@@ -0,0 +1,5 @@
+from main import calculator
+
+# test_example.py
+def test_addition():
+    assert calculator(1,1,"+") == 2
